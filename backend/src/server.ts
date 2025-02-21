84abd9c5167357a222076889061ff4d308d4c2f1
prolazycoder <vaibhav.sinha.phe21@itbhu.ac.in>
2025-02-21 17:34:22 +0530

    debugging by harcoding

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,4 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(
+    `✅ Backend deployed at: ${process.env.YOUR_BACKEND_URL || "Not Set"}`
+  );
 }); 
